0637f126d6eea8fa1d37b7c5be5bdda5143d2610
Eric Yeung <59549511+eric-yeung@users.noreply.github.com>
2020-02-19 16:20:46 -0500

    Delete a5.java

--- a5.java
+++
@@ -1,8 +0,0 @@
-public class A5{
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new FileReader("A5.tiny"));
-		String line = br.readLine();
-		BufferedReader bw = new BufferedReader(new FileWriter("a5.output"));
-		bw.close()
-	}
-}
